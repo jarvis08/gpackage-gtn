21c0ba6fad20d61e8dfc4099433a8b62a11c0184
jarvis08 <jarvis08.cdb@gamil.com>
2021-03-07 22:38:10 +0900

    add script for installing libraries

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+export TORCH=1.7.0
+export CUDA=cu101
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-geometric
